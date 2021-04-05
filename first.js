29430b99ec26480f49f2ed2f9123468863fae274
52Tellur <81295209+52Tellur@users.noreply.github.com>
2021-04-05 10:11:19 +0900

    Update first.js

--- first.js
+++ first.js
@@ -8,36 +8,14 @@ class Scratch3NewBlock {
                 {
                     opcode: 'trueblk',
                     blockType: Scratch.BlockType.REPORTER,
-                    text: '[TEXT]を返す',
-                    arguments: {
-                        TEXT: {
-                            type: ArgumentType.STRING,
-                            defaultValue: "true",
-                            menu: "moji"
-                        }
-                    }
+                    text: 'trueを返す',
                 }
             ],
-            menus: {
-                moji: {
-                    acceptReporters: true,
-                    items: [{ text: "true", value: "true"}, {text: "false", value: "false"},{ text: "Infinity", value: "Infinity"},{ text: "NaN", value: "NaN"}]
-                }
-            }
         };
     }
 
-    trueblk({TEXT}) {
-        if TEXT="true"{
-            return true;
-        } else if TEXT="false"{
-            return false;
-        }else if TEXT="Infinity"{
-            return Infinity;
-        }else {
-            return NaN;
-        }
-        
+    trueblk() {
+        return true;
     }
 
 Scratch.extensions.register(Sratch3NewBlock());
