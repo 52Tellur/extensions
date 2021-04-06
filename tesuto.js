d762ddf9d33c5479bd03b85485dd8aa3e570f331
52Tellur <81295209+52Tellur@users.noreply.github.com>
2021-04-06 20:52:27 +0900

    Create tesuto.js

---
+++ tesuto.js
@@ -0,0 +1,48 @@
+class Tellur {
+  constructor() {}
+  getInfo() {
+    return {
+     id: 'Tellur',
+     name: '返す系',
+     blocks: [
+       {
+         opcode: 'same',
+         blockType: Scratch.BlockType.BOOLEAN,
+         text: '[X]と[N]は同じ', 
+         arguments: {
+           X: {
+             type: "string",
+             defaultValue: '2'
+           },
+           N: {
+             type: "string",
+             defaultValue: '3'
+           }
+         }
+       },
+       {
+         opcode: 'jyou',
+         blockType: Scratch.BlockType.REPORTER,
+         text: '[X]の[N]乗', 
+         arguments: {
+           X: {
+             type: "number",
+             defaultValue: '3'
+           },
+           N: {
+             type: "number",
+             defaultValue: '2'
+           }
+         }
+       }
+     ]
+   }
+ }
+  same({X,N}) {
+   return X === N;
+  }
+  jyou(args){
+   return Math.pow(args.X, args.N); 
+  }
+}
+Scratch.extensions.register(new Tellur());
