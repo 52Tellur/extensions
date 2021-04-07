1bf8376384c3ac4ea132bf90570fbded19f151b7
52Tellur <81295209+52Tellur@users.noreply.github.com>
2021-04-07 15:26:43 +0900

    Update Tellur.js

--- Tellur.js
+++ Tellur.js
@@ -4,8 +4,8 @@ class Tellur {
     return {
       id: 'Tellur',
       name: 'Tellur',
-      color1: '#A682FF',
-      color2: '#7BB8EE',
+      color1: '#999',
+      color2: '#FFF',
       color3: '#000',
      blocks: [
        {
